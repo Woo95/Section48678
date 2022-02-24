46f84f9a9eb62f49b56bb8f04721aab8c119c613
5900w <chaewan2002@gmail.com>
2022-02-24 22:41:32 +0900

    finished playscene.h

--- Lab5/src/PlayScene.h
+++ Lab5/src/PlayScene.h
@@ -24,8 +24,10 @@ private:
 	// IMGUI Function
 	void GUI_Function();
 	std::string m_guiTitle;
-
 	glm::vec2 m_mousePosition;
+	bool m_isGridEnabled;
+	static int start_position[2];
+	static int goal_position[2];
 
 	// Game Objects
 	Target* m_pTarget;
@@ -33,12 +35,18 @@ private:
 
 	// Pathfinding Objects and Functions
 	std::vector<Tile*> m_pGrid;
-	bool m_isGridEnabled;
-
 	void m_buildGrid();
 	void m_setGridEnabled(bool state);
 	bool m_getGridEnabled() const;
 	void m_computeTileCosts();
+	void m_findShortestPath(); // Our big algorithm
+	void m_displayPathList(); // In console
+	void m_resetPathfinding();
+
+	// Tile lists for pathfinding
+	std::vector<Tile*> m_pOpenList;
+	std::vector<Tile*> m_pClosedList;
+	std::vector<Tile*> m_pPathList;
 
 	// convenience functions
 	Tile* m_getTile(int col, int row);
@@ -46,6 +54,11 @@ private:
 
 	// heuristic
 	Heuristic m_currentHeuristic;
+
+	// Ship movement
+	int m_moveCounter = 0;
+	bool m_shipIsMoving = false;
+	void m_moveShip();
 };
 
 #endif /* defined (__PLAY_SCENE__) */
