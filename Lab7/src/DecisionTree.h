ce2bd00caa4d08759d575aa7f83271aa5e767860
5900w <chaewan2002@gmail.com>
2022-03-25 02:21:39 +0900

    Created DecisionTree class

---
+++ Lab7/src/DecisionTree.h
@@ -0,0 +1,47 @@
+#pragma once
+#ifndef __DECISION_TREE__
+#define __DECISION_TREE__
+#include "Agent.h"
+#include "CloseCombatCondition.h"
+#include "LOSCondition.h"
+#include "RadiusCondition.h"
+#include "TreeNode.h"
+#include "TreeNodeType.h"
+
+class DecisionTree
+{
+public:
+	// constructor and destructor
+	DecisionTree();
+	DecisionTree(Agent* agent); // New overload for Lab 7.
+	~DecisionTree();
+
+
+	// getters and setters
+	Agent* getAgent() const;
+	LOSCondition* getLOSNode() const;
+	RadiusCondition* getRadiusNode() const;
+	CloseCombatCondition* getCloseCombatNode() const;
+	void setAgent(Agent* agent);
+
+	// convenience functions
+	TreeNode* AddNode(TreeNode* parent, TreeNode* child_node, TreeNodeType type);
+	void Display();
+	void Update();
+	void clean();
+
+	void MakeDecision(); // in order traversal
+private:
+	Agent* m_agent;
+
+	LOSCondition* m_LOSNode;
+	RadiusCondition* m_RadiusNode;
+	CloseCombatCondition* m_CloseCombatNode;
+
+	void m_buildTree();
+
+	std::vector<TreeNode*> m_treeNodeList;
+};
+
+
+#endif /* defined (__DECISION_TREE__) */
